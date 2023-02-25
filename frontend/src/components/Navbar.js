14256e40b5b4c18accbe9858b11b29f4b2f96fac
saarYul <saar.yulevich@auroralabs.com>
2023-02-25 22:16:17 +0200

    add category filter

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Menu, Input, Icon, Label } from 'semantic-ui-react'
+import { Menu, Input, Icon, Label, Dropdown, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import _ from 'lodash'
-
+import logo from '../images/Logo.jpg'   
 import './Navbar.css'
 
 class Navbar extends React.Component {
@@ -16,6 +16,7 @@ class Navbar extends React.Component {
 
         this.handleOnScroll = this.handleOnScroll.bind(this)
         this.handleSearchItems = this.handleSearchItems.bind(this)
+        this.handleCategoryItems = this.handleCategoryItems.bind(this)
     }
 
     componentDidMount() {
@@ -49,13 +50,63 @@ class Navbar extends React.Component {
         this.props.filterProducts(filteredProducts)
     }
 
+    handleCategoryItems(e, data) {
+        if (data.value != 'All'){
+            let filteredProducts = _.filter(this.state.initialProducts, (obj) => {
+                return _.includes(obj.category.toLowerCase(), data.value.toLowerCase())
+            })
+            this.props.filterProducts(filteredProducts)
+        }
+        else{
+            let filteredProducts = _.filter(this.state.initialProducts, (obj) => {
+                return _.includes(obj.category.toLowerCase(), '')
+            })
+            this.props.filterProducts(filteredProducts)      
+        }
+    }
+
     render() {
+        const categoryOptions = [
+            {
+              key: 'Cookies',
+              text: 'Cookies',
+              value: 'Cookies',
+            },
+            {
+              key: 'Cakes',
+              text: 'Cakes',
+              value: 'Cakes',
+            },
+            {
+              key: 'Breads',
+              text: 'Breads',
+              value: 'Breads',
+            },
+            {
+                key: 'All',
+                text: 'All',
+                value: 'All',
+              }
+        ]
         return (
             <Menu id='navbar' className={this.state.className} size='massive' fixed='top' secondary>
                 <Menu.Item id='logo' as={Link} to='/' header>
-                    Logo
+                    <img src={logo} as={Link} to='/' header/>
                 </Menu.Item>
-
+                <Menu.Item id='logoText' as={Link} to='/' header>
+                    KKCake
+                </Menu.Item>
+                <Menu.Item id='category'>
+                    <Dropdown
+                        placeholder='Select Category'
+                        fluid
+                        selection
+                        label='Category'
+                        name='Category'
+                        onChange={this.handleCategoryItems}
+                        options={categoryOptions}
+                    />
+                </Menu.Item>
                 <Menu.Menu position='right'>
                     <Menu.Item>
                         <Input transparent icon={{ name: 'search', link: true }} placeholder='Search...' style={this.props.showSearch ? null : {display: 'none'}} onChange={this.handleSearchItems} />
